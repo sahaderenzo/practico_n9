f0cdd0d6e0fb2965600e009ddd3845ca42a85da1
SahadeRenzo <sahaderenzo02@gmail.com>
2024-11-12 14:24:37 -0300

    Finalizando quinto video

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
 import { MiPrimerComponent } from "./components/MiPrimerComponente/MiPrimerComponent"
+import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect"
 
 export const App = () => {
+
     return (
-        <>
+        <div style={{ display: "flex", flexDirection: "column", gap: "2vh" }}>
             <MiPrimerComponent text={"Texto de prueba"} color={"red"} fontSize={5} />
             <ComponentCounter />
-        </>
+            <ComponentUseEffect />
+        </div>
     )
 }
